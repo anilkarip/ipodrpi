76dd1eee1af96d5b045af80f3b91bd01267ab905
anilkarip <67373164+anilkarip@users.noreply.github.com>
2022-06-10 22:14:57 +0300

    update for current display options and usbdac

--- cli.sh
+++ cli.sh
@@ -2,58 +2,6 @@
 
 #screen
 
-echo -e "\e[1;36m iPod Raspberry Pi with 2 inch LCD and CLICKWHEEL installer  \e[0m"
-echo -e "\e[1;36m by Niels Bor @ DARKMATTERWORKS  \e[0m"
-
-read -r -p "OSMC? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-echo -e "\e[1;36m DISABLING OSMC REPOSITORY (BECAUSE OF LIBC6 ERROR)  \e[0m"
-sudo bash -c 'echo "deb http://mirrordirector.raspbian.org/raspbian buster main contrib non-free rpi" > /etc/apt/sources.list'
-else
-echo -e "\e[1;36m SUPPOSING WE ARE ON RASPBIAN  \e[0m"
-fi
-
-read -r -p "Add Adding 2inch display parameters to /boot/config.txt ? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-echo -e "\e[1;36m Ok, writing to /boot/config.txt   \e[0m"
-sudo bash -c 'echo "enable_uart=1" >> /boot/config.txt'
-sudo bash -c 'echo "hdmi_force_hotplug=1" >> /boot/config.txt'
-sudo bash -c 'echo "hdmi_cvt=320 240 60 1 0 0 0" >> /boot/config.txt'
-sudo bash -c 'echo "hdmi_group=2" >> /boot/config.txt'
-sudo bash -c 'echo "hdmi_mode=1" >> /boot/config.txt'
-sudo bash -c 'echo "hdmi_mode=87" >> /boot/config.txt'
-sudo bash -c 'echo "display_rotate=0" >> /boot/config.txt'
-else
-echo Skipping..
-fi
-
-echo -e "\e[1;36m APT UPDATE & UPGRADE  \e[0m"
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-
-echo -e "\e[1;36m Waveshare_fbcp DRIVER FOR 2INCH DISPLAY  \e[0m"
-sudo apt-get install cmake p7zip-full gcc git build-essential -y
-sudo mkdir /opt/vc/include
-git clone https://github.com/syproduction/ipodrpi
-sudo cp -r ~/ipodrpi/include /opt/vc/
-
-cd ~
-wget https://www.waveshare.com/w/upload/8/8d/Waveshare_fbcp-main.7z
-7z x Waveshare_fbcp-main.7z
-cd waveshare_fbcp-main
-mkdir build
-cd build
-cmake -DSPI_BUS_CLOCK_DIVISOR=20 -DWAVESHARE_2INCH_LCD=ON -DBACKLIGHT_CONTROL=ON -DSTATISTICS=0 ..
-make -j
-sudo ./fbcp&
-
-echo -e "\e[1;36m DISPLAY SHOULD SHOW SOMETHING NOW \e[0m"
-echo -e "\e[1;36m DISPLAY AUTOSTART ON BOOT (/etc/rc.local METHOD) \e[0m"
-sudo cp ~/waveshare_fbcp-main/build/fbcp /usr/local/bin/fbcp
-sudo sed -i -e '$ i\fbcp&' /etc/rc.local
-
 echo -e "\e[1;36m NOW INSTALL pikeyd \e[0m"
 cd ~
 sudo cp ~/ipodrpi/etc/pikeyd.conf /etc/pikeyd.conf
@@ -91,37 +39,6 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
 sudo cp ~/ipodrpi/system/console-setup /etc/default/console-setup
 sudo /etc/init.d/console-setup.sh restart
-else
-echo Skipping..
-fi
-
-read -r -p "CREATIVE SOUNDBLASTER PLAY! ALSA SETUP ? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-sudo cp ~/ipodrpi/system/alsa-base.conf /etc/modprobe.d/alsa-base.conf
-else
-echo Skipping..
-fi
-
-read -r -p " CMUS ? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-echo -e "\e[1;36m OK \e[0m"
-sudo apt-get install cmus -y
-sudo rm /etc/pikeyd.conf
-sudo cp ~/ipodrpi/etc/pikeyd.conf.cmus /etc/pikeyd.conf
-cmus &
-cmus-remote -C "bind -f common u shell ~/.config/cmus/cmus-update.sh" &
-cmus-remote -C "set softvol=true" &
-sudo killall cmus
-sudo mkdir ~/Music
-sudo cp ~/ipodrpi/system/rc ~/.config/cmus/rc
-sudo cp ~/ipodrpi/system/cmus-update.sh ~/.config/cmus/cmus-update.sh
-sudo chmod +x ~/.config/cmus/cmus-update.sh
-echo "cmus" >> ~/.bashrc
-#echo "~/.config/cmus/cmus-update.sh" >> ~/.bashrc
-echo -e "\e[1;36m IF NO SOUND IN CMUS, SET IT UP IN RASPI-CONFIG \e[0m"
-echo -e "\e[1;36m AND SET UP AUTOLOGIN IN RASPI-CONFIG \e[0m"
 else
 echo Skipping..
 fi
